89900afd4adda5d094ccc9a3441c472eec8d254c
Sanchit Shahi <sanchitshahi00@gmail.com>
2026-07-17 08:05:13 +0545

    update

--- app.js
+++
@@ -1,7 +0,0 @@
-createCapsule(
-    "College Memory",
-    "Learning JavaScript for Time Capsule",
-    "2027-01-01"
-);
-
-showCapsules();
--- display.js
+++
@@ -1,7 +0,0 @@
-function showCapsules() {
-
-    console.log("All Capsules:");
-
-    console.log(capsules);
-
-}
